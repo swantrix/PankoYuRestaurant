743a6b677219548eafcca32b72f637322e1cbfde
Annemijn van Koten <annemijnvankoten@gmail.com>
2022-03-25 10:54:35 +0100

    Tried to fix errors

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var path = require('path');
 var morgan = require('morgan');
 var sqlite3 = require('sqlite3').verbose();
 var http = require('http');
+var url = require('url');
+
+var router = express.Router();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -64,7 +67,7 @@ var staticPath = path.join(__dirname, "static");
 app.use(express.static(staticPath));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.resolve(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.json());
@@ -72,17 +75,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Routers
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
 //Error handling
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*app.use(function(req, res, next) {
   next(createError(404));
-});
+});*/
 
 // error handler
-app.use(function(err, req, res, next) {
+/*app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -90,12 +97,14 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
-});
+});*/
 
-http.createServer(function (req, res) {
+/*http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     
     res.end();
-  }).listen(8018);
+  }).listen(8018);*/
+
+app.listen(8018);
 
 module.exports = app;
